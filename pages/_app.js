bc2c7637b99e943719726eb61cb8b866eba088cb
Arindam Halder <halderarindam10000@gmail.com>
2023-02-24 23:38:01 +0530

    :bug: site desc updated
    site desc updated

--- pages/_app.js
+++ pages/_app.js
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }) {
           />
           <meta
             name="keywords"
-            content="Srijan, cultural, ju, fest, srijanju, technology, events, games, coding"
+            content="Srijan, cultural, ju, fest, srijanju, technology, events, games, coding, srijan23, techfest"
           />
           <meta name="author" content="FETSU" />
           <link rel="manifest" href="manifest.json" />
@@ -25,10 +25,10 @@ export default function App({ Component, pageProps }) {
           <meta property="og:title" content="Srijan23" />
           <meta
             property="og:description"
-            content="The annual techno management fest of Jadavpur University"
+            content="The wait is over for the 15th edition of SRIJAN, Jadavpur University, Kolkata's largest and most awaited Techno-Management Fest."
           />
           <meta property="og:url" content="https://srijanju.in" />
-          <meta property="og:site_name" content="Srijan23" />
+          <meta property="og:site_name" content="SRIJAN'23 | Jadavpur University" />
           <meta property="og:image" itemProp="image" content="https://srijanju.in/favicon.ico"/>
           <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
           <link rel="icon" type="image/x-icon" href="favicon.ico" />
