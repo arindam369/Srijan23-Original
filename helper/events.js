7ff25d53c6a4a5576f21b13fa855bd7a071624ac
AnuragJha003 <97457228+AnuragJha003@users.noreply.github.com>
2023-03-20 17:23:25 +0530

    Update events.js

--- helper/events.js
+++ helper/events.js
@@ -101,6 +101,24 @@ export const events = [
         eventRules: ["3-5 players per team and 6 different levels", "A total of 12 teams will go past through the first round from the total unlimited teams based on their design skills", "The value of points are limited to the specific level and are not carried over", "Teams will not be allowed compensation for time wasting", "A volunteer can disqualify a participant or team for breaking the decorum if deemed viable", "There are total 6 stages in this game.", "Please click upon the 'Click Here' button below to access the UPSG RULE BOOK"],
         eventRuleLink: "https://docs.google.com/presentation/d/1seV_fGmrzPuO3QcC6HuJaTFc96ZTTEd2/edit#slide=id.p1"
     },
+     {
+        eventId: "007",
+        eventName: "Climbon'23",
+        eventDate: {prelims: ["April 15, 2023"], finals: "April 16, 2023"},
+        // eventTime: {prelims: ["10:30AM - 4PM"], finals: "2PM - 5PM"},
+        eventPoster: "/assets/events/ClimbOn.jpg",
+        eventType: "Mountaineering-Hiking",
+        eventHashtags: ["Mountaineering", "Rocket-Climbing"],
+        eventCoordinators: ["Krishna Mittal  [ 8540062753 ]", "Urwah Jawaid [ 9103158993 ]", "Ashish Kumar Mandal [ 72949 25744 ]"],
+        // eventVenue: "SMCC - 4th Floor - Lab",
+        minMembers: 1,
+        maxMembers: 1,
+        // eventShortDescription: "Deciphering their way through clues and challenges set in the world of binary, the fastest team to the Treasure wins",
+        eventDescription: ["CLIMBON is an artificial rock climbing competition organized by Jadavpur University Mountaineering And Hiking Club every year. It is open to all and there is no age or gender bar. No prior experience is required. Any passionate person from any college can participate. The competition would take place under the supervision of esteemed instructors and all the safety gears would also be provided. It is the best place for any adventurer to begin."],
+        eventRules: ["Climbing routes: The competition may have different climbing routes with varying levels of difficulty, which are marked with colored holds. Climbers must follow the designated route and only use the marked holds. Touching any other part of the wall or using any other holds is not allowed and may result in disqualification.", "Timing: Each climber has a set amount of time to complete the route. The time limit may vary depending on the difficulty level of the route. If the climber does not complete the route within the specified time, their score will be based on the furthest point they reached.", "Scoring: The score is determined by the number of holds the climber reaches and touches during the climb. Climbers are awarded points for each hold they touch. The climber with the highest score in their category wins.","Categories: Climbers may be grouped into different categories based on their gender. Each category may have different routes and scoring criteria.","Safety: Safety is of utmost importance in artificial rock climbing contests. Climbers must follow all safety guidelines and instructions provided by event organizers. Judges may stop a climber's ascent if they feel that the climber is in danger or not following safety rules.","Sportsmanship: Climbers are expected to display good sportsmanship and respect for fellow competitors. Any unsportsmanlike behavior or actions that endanger others may result in disqualification.","Equipment: We will provide all the necessary equipment for the competition, but climbers are encouraged to wear well-fitting shoes with a thin sole and good grip, such as PT shoes."],
+        //eventRules: ["CLIMBON is an artificial rock climbing competition organized by Jadavpur University Mountaineering And Hiking Club every year. It is open to all and there is no age or gender bar. No prior experience is required. Any passionate person from any college can participate. The competition would take place under the supervision of esteemed instructors and all the safety gears would also be provided. It is the best place for any adventurer to begin."],
+        eventRuleLink: "https://drive.google.com/file/d/1LUraTyqbn8HVO2_sKE_Dh7nvUfi_hJas/view"
+    },
     
     // {
     //     eventId: "015",
