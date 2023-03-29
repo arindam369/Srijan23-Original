3341e358d36c4cc6f64863159a4b2388024a2a87
DEBAJIT SARKAR <halderarindam10000@gmail.com>
2023-03-29 19:58:35 +0530

    event: 011 escapeRoom added

--- components/PolarChart.jsx
+++ components/PolarChart.jsx
@@ -30,7 +30,7 @@ export default function PolarChart() {
         labels: ['Coding', 'Gaming', 'Circuits and Robotics', 'Business and Management', 'Brainstorming', 'Misc'],
         datasets: [
             {
-                data: [0, 1, 1, 3, 1, 4],
+                data: [0, 1, 1, 3, 2, 4],
                 backgroundColor: [
                     '#29b6f6',
                     '#00e676',
