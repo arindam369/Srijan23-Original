56259d9ecea9f99e6ecdb7f1a57ffa30c3f0a46d
Arindam Halder <halderarindam10000@gmail.com>
2023-04-10 19:28:29 +0530

    ayush bhaiya ko access de diya

--- helper/admins.js
+++ helper/admins.js
@@ -20,8 +20,8 @@ export const admins = {
     "019": ["halderarindam10000", "anusha_sbgs"],
     "020": ["halderarindam10000", "soumyadeeppal238", "tuhinchakraborty6000"],
     "021": ["halderarindam10000", "soumyadeeppal238", "tuhinchakraborty6000"],
-    "022": ["halderarindam10000", "dibyabrata1234", "aarindey", "arp23359"],
-    "023": ["halderarindam10000", "priyamjucode", "chiragjaiswal454"],
-    "024": ["halderarindam10000"],
+    "022": ["halderarindam10000", "dibyabrata1234", "aarindey", "arp23359", "kayush479"],
+    "023": ["halderarindam10000", "priyamjucode", "chiragjaiswal454", "kayush479"],
+    "024": ["halderarindam10000", "kayush479"],
     "025": ["halderarindam10000"],
 }
